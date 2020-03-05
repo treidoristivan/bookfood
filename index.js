44a5983cc3ba8ef131b08f4609e9fce5544cc5a9
treidoristivan <ristivantreido@gmail.com>
2020-03-05 15:12:15 +0700

    Add Set and Import Routes Restaurants

--- index.js
+++ index.js
@@ -2,24 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
-const { User } = require('./src/routes/users')
-const { Auth } = require('./src/routes/auth')
-const { checkAuthToken } = require('./src/middleware/AuthMiddleware')
-const { Migration } = require('./src/routes/migrations')
+/* Import ROUTES */
+const { Users } = require('./src/routes/users')
+const { Restaurants } = require('./src/routes/restaurants')
 
-// Middleware
+/* Middleware */
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// Router
-app.use('/users', checkAuthToken, User)
-app.use('/auth', Auth)
-app.use('/migrate', Migration)
+/* Set ROUTES */
+app.use('/users', Users)
+app.use('/restaurants', Restaurants)
 
-app.get('/', (req, res) => {
-  res.send('Server Is Running')
-})
-
-app.listen(1111, () => {
-  console.log('Server Running at Port BOOK & FOOD')
+/* Server Listen */
+const PORT = 4444
+app.listen(PORT, () => {
+  console.log('Server Listen on Port ' + PORT)
 })

1334ff948744eb80d32f35d90277311ff96431cc
treidoristivan <ristivantreido@gmail.com>
2020-03-05 17:04:32 +0700

    Add module Patch Restaurants

--- src/controllers/restaurants.js
+++ src/controllers/restaurants.js
@@ -1,13 +1,13 @@
 // const bcrypt = require('bcryptjs')
 // const jwt = require('jsonwebtoken')
 // const { runQuery } = require('../config/db')
-const { GetRestaurants } = require('../models/restaurants')
-const { GetUsers } = require ('../models/users')
+const { GetRestaurants, GetDetailRestaurants, UpdateRestaurans } = require('../models/restaurants')
+const { GetUsers } = require('../models/users')
 // const { validateUsernamePassword } = require('../utility/validate')
 
 module.exports.GetRestaurants = async (req, res, next) => {
   try {
-    const dataRestaurants = await dataRestaurants(false, {p: 'ram'})
+    const dataRestaurants = await dataRestaurants (false, { p: 'ram' })
     res.status(100).send({
       success: true,
       data: dataRestaurants
@@ -37,18 +37,40 @@ module.exports.GetDetailRestaurants = async (req, res, next) => {
 }
 module.exports.UpdateRestaurants = async (req, res, next) => {
   try {
-    if(!(Object.keys(req.body).length > 0)) {
+    if (!(Object.keys(req.body).length > 0)) {
       throw new Error('Insert column want to Update')
     }
     const { id } = req.params
     const dataRestaurants = await GetRestaurants(id)
     const dataOwner = await GetUsers(req.auth.id)
-    if(!dataRestaurants) {
+    if (!dataRestaurants) {
       throw new Error('Restaurants Not Availabe')
     }
-    if (!(dataOwner.id === dataRestaurants.idOwner ||  dataOwner.isSuperowner))
-  } catch {
-
+    if (!(dataOwner.id === dataRestaurants.idOwner || dataOwner.isSuperowner)) {
+      res.status(200).send({
+        success: false,
+        msg: 'Login with Admin  to Updated This Restaurants'
+      })
+    }
+    const params = Object.keys(req.body).map((v) => {
+      if (v && ['name', 'logo', 'location', 'description'].includes(v) && req.body[v]) {
+        return { key: v, value: req.body[v] }
+      } else {
+        return null
+      }
+    }).filter(v => v)
+    const Update = await UpdateRestaurants(id, params)
+    if (update) {
+      res.status(100)({
+        success: true,
+        msg: 'Success Updated Restaurants'
+      })
+    }
+  } catch (e) {
+    res.status(200)({
+      success: false,
+      msg: e.message
+    })
   }
 }
 // exports.RegisterRestaurants = async (req, res, next) => {
@@ -80,15 +102,15 @@ module.exports.UpdateRestaurants = async (req, res, next) => {
 //     })
 //   }
 //}
-exports.UpdateRestaurans = async (req, res, next) => {
-  try {
-const  { name } = req.params
-if (name) {
-  if (name.val) {
-} else {
+// exports.UpdateRestaurans = async (req, res, next) => {
+//   try {
+// const  { name } = req.params
+// if (name) {
+//   if (name.val) {
+// } else {
 
-}
-  } 
-} catch {
+// }
+//   } 
+// } catch {
 
-}
+// }

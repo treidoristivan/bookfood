2d32f92d6d86c49f64b9d996dd20e81d341ae1a7
treidoristivan <ristivantreido@gmail.com>
2020-03-06 00:18:45 +0700

    Add features Set and Import Route for items

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 /* Import ROUTES */
 const { Users } = require('./src/routes/users')
 const { Restaurants } = require('./src/routes/restaurants')
+const { Items } = require('./src/routes/items')
 
 /* Middleware */
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -13,6 +14,7 @@ app.use(bodyParser.json())
 /* Set ROUTES */
 app.use('/users', Users)
 app.use('/restaurants', Restaurants)
+app.use('/items', Items)
 
 /* Server Listen */
 const PORT = 4444

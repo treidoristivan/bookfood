77e9e9dfa03241b5c6a93aa5e52b32f908c53ef6
treidoristivan <ristivantreido@gmail.com>
2020-03-05 08:45:55 +0700

    Export Getprofile and DeleteUser

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,204 +1,148 @@
-const users = require('../models/users')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { runQuery } = require('../config/db')
+const { GetUser, RegisterUser, UpdateProfile, GetProfile, DeleteUser } = require('../models/users')
+const { validateUsernamePassword } = require('../utility/validate')
 
-require('dotenv').config()
-
-const GetAllUsers = async (req, res) => {
+exports.GetProfile = async (req, res, next) => {
   try {
-    //  Default Condition
-    const params = {
-      curentPage: req.query.page || 1,
-      perPage: req.query.limit || 5,
-      search: req.query.search || null,
-      sort: req.query.sort || [{ keys: 'name', value: 0 }]
-    }
-
-    // Get Data
-    const data = await users.get(false, params)
-
-    // Send Data
-    if (data) {
-      res.send({
+    const profileUser = await GetProfile(req.auth.id)
+    if (profileUser) {
+      res.status(200).send({
         success: true,
-        data
+        data: profileUser
       })
+    }
+    throw new Error('Something Wrong')
+  } catch (e) {
+    res.status(202).send({
+      success: false,
+      msg: e.message
+    })
+  }
+}
+exports.RegisterUser = async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    if (username && password) {
+      const validate = validateUsernamePassword(username, password)
+      if (validate.val) {
+        const hashPassword = bcrypt.hashSync(password)
+        const statusRegister = await RegisterUser({ username, password: hashPassword })
+        if (statusRegister) {
+          res.status(201).send({
+            success: true,
+            msg: 'Register Success, Please Login'
+          })
+        }
+      } else {
+        throw new Error(validate.message)
+      }
     } else {
-      res.send({
-        success: false,
-        data: []
-      })
+      throw new Error('Username and Password is Required')
     }
   } catch (e) {
     console.log(e)
-    res.send({
+    res.status(202).send({
       success: false,
-      data: false
+      msg: e.message
     })
   }
 }
 
-const GetDetailUsers = async (req, res) => {
+exports.LoginUser = async (req, res, next) => {
   try {
-    var { id } = req.params
-    const data = await users.get(id)
-    if (data) {
+    const { username, password } = req.body
+    if (username && password) {
+      const dataLogin = await new Promise((resolve, reject) => {
+        runQuery(`SELECT _id,username,password FROM users WHERE username='${username}'`,
+          (err, results) => {
+            if (!err && results[1].length > 0 && bcrypt.compareSync(password, results[1][0].password)) {
+              const userData = { id: results[1][0]._id, username }
+              resolve(userData)
+            } else {
+              reject(new Error(err || 'Username Or Password Wrong'))
+            }
+          })
+      })
+      const token = jwt.sign(dataLogin, process.env.APP_KEY, { expiresIn: '1H' })
       res.send({
         success: true,
-        data
+        msg: 'Login Success',
+        data: {
+          token
+        }
       })
     } else {
-      throw new Error('empty')
+      throw new Error('Username and Password is Required')
     }
   } catch (e) {
-    res.send({
+    console.log(e)
+    res.status(401).send({
       success: false,
-      msg: `The is no Users with id ${id}`
-    })
-  }
-}
-const CreateUsers = async (req, res) => {
-  const { fullName, usersname, password } = req.body
-  const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-  try {
-    const create = await users.create(fullName, usersname, hashPassword)
-    if (create) {
-      res.send({
-        success: true,
-        msg: 'Users has been created'
-      })
-    } else {
-      res.send({
-        success: false,
-        msg: 'Users already exists'
-      })
-    }
-  } catch (e) {
-    res.send({
-      success: false,
-      msg: e.msg
+      msg: e.message
     })
   }
 }
 
-const UpdateUsers = async (req, res) => {
-  const { id } = req.params
-  const key = Object.keys(req.body)
-  const filllable = ['name', 'usersname', 'old_password']
-  let params = key.map((v, i) => {
-    if (v && filllable.includes(key[i])) {
-      if (req.body[key[i]]) {
-        return { keys: v, value: req.body[v] }
-      } else {
-        return null
-      }
+exports.UpdateUser = async (req, res, next) => {
+  const { id } = req.auth
+  const fillable = ['username', 'fullname', 'email', 'gender', 'address', 'picture']
+  const params = Object.keys(req.body).map((v) => {
+    if (v && fillable.includes(v) && req.body[v]) {
+      return { keys: v, value: req.body[v] }
     } else {
       return null
     }
   }).filter(o => o)
   try {
     if (req.body.old_password) {
-      /* Condition if users sending old_password at POST request */
-
-      /* Get users data by Id parameter */
-      const GetUsers = await users.get(id)
-
-      /* Get users old password */
-      const oldPassword = GetUsers.password
-
-      /* variable checking wheter hash match or not */
-      const compare = bcrypt.compareSync(req.body.old_password, oldPassword)
-
-      /* Comparing new password and confirm password request */
-      if (req.body.new_password === req.body.confirm_password) {
-        if (compare) {
-          /* If  hash is match */
-
-          /* Changing old password parameter for database to password with new password hash */
-          params = params.map(o => {
-            if (o.keys === 'old_password') {
-              return { keys: 'password', value: bcrypt.hashSync(req.body.new_password) }
-            } else {
-              return o
-            }
-          })
-
-          /* Update password with model */
-          const update = await users.update(id, params)
-
-          /* If Update success or Not */
-          if (update) {
-            res.send({
-              success: true,
-              msg: `Users With id ${id} has been updated`
-            })
-          } else {
-            res.send({
-              success: false,
-              msg: 'Failed to update users!'
-            })
-          }
-        } else {
-          res.send({
-            success: false,
-            msg: 'Wrong Old Password'
-          })
-        }
-      } else {
-        res.send({
-          success: false,
-          msg: ' Confirm Password Not Match'
-        })
+      const user = await GetUser(id)
+      const oldPassword = user.password
+      const { NewPassword, ConfirmPassword } = req.body
+      if (!(NewPassword && ConfirmPassword)) {
+        throw new Error('New Password or Confirm Password Not Defined')
       }
-    } else {
-      /* Update password with model */
-      const update = await users.update(id, params)
-
-      /* If Update success or Not */
-      if (update) {
-        res.send({
-          success: true,
-          msg: `Users With id ${id} has been updated`
-        })
-      } else {
-        res.send({
-          success: false,
-          msg: 'Failed to update users!'
-        })
+      if (!(NewPassword === ConfirmPassword)) {
+        throw new Error('Confirm Password Not Match')
       }
+      if (!(bcrypt.compareSync(req.body.old_password, oldPassword))) {
+        throw new Error('Old Password Not Match')
+      }
+      params.push({ keys: 'password', value: bcrypt.hashSync(NewPassword) })
     }
-  } catch (e) {
-    res.send({
-      success: false,
-      msg: e.message || 'Internal Server Error'
-    })
-  }
-}
-const DeleteUsers = async (req, res) => {
-  try {
-    const { id } = req.body
-    if (await users.delete(parseInt(id))) {
+    const update = await UpdateProfile(id, params)
+    if (update) {
       res.send({
         success: true,
-        msg: `Users with id ${id} has been deleted`
+        msg: `User ${req.auth.username} has been updated`
       })
     } else {
-      res.send({
-        success: false,
-        msg: 'Failed to delete users'
-      })
+      throw new Error('Failed to update user!')
     }
   } catch (e) {
-    res.send({
+    console.log(e)
+    res.status(202).send({
       success: false,
-      msg: 'Internal Server Error'
+      msg: e.message
     })
   }
 }
 
-module.exports = {
-  GetAllUsers,
-  GetDetailUsers,
-  CreateUsers,
-  UpdateUsers,
-  DeleteUsers
+exports.DeleteUser = async (req, res, next) => {
+  const { id } = req.auth
+  try {
+    if (!(await DeleteUser(id))) {
+      throw new Error('Failed to Delete User')
+    }
+    res.status(200).send({
+      success: true,
+      msg: 'Success Delete Your Account'
+    })
+  } catch (e) {
+    res.status(202).send({
+      success: false,
+      msg: e.message
+    })
+  }
 }

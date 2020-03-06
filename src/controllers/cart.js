281df18d6f6d1ff7c86b0048318123aaabfbfbc4
treidoristivan <ristivantreido@gmail.com>
2020-03-06 16:55:45 +0700

    Add GetCart, AddItems for exports Cart

---
+++ src/controllers/cart.js
@@ -0,0 +1,57 @@
+const { getCart, AddItems } = require('../models/cart')
+const { GetItems} = require('../models/items')
+exports.GetAllCart = async (req, res, next) => {
+  try {
+    const { id } = req.auth
+    const cart = await getCart(id)
+    if (cart.length > 0) {
+      return res.status(100).send({
+        success: true,
+        msg: cart
+      })
+    } else {
+      return res.status(100).send({
+        success: false,
+        msg: 'Data Unkown'
+      })
+    }
+  } catch (e) {
+    console.log(e)
+    res.status(200).send({
+      success: false,
+      msg: e.message
+    })
+  }
+}
+exports.AddItems = async (req, res, next) => {
+  try {
+    if (!req.body.id_item || !req.body.totalItems) {
+      throw new Error('Is Not Available')
+    }
+    const idUser = req.auth.id
+    const items = await GetItems(req.body.id_item)
+    if (!items) {
+      throw new Error(`${req.body.id_item} Is Not Available`)
+    }
+    const dataItems = {
+      idItems: items.id,
+      nameItems: items.name,
+      totalItems: req.body.totalItems,
+      totalPrice: parseFloat(req.body.totalItems) * parseFloat(items.price)
+    }
+    const addedItems = await AddItems(idUser, dataItems)
+    if (addedItems) {
+      return res.status(100)({
+        success: true,
+        msg: 'Success To Add Items in Cart'
+      })
+    }
+    throw new Error('Failed To Add Items in Cart')
+  } catch (e) {
+    console.log(e)
+    res.status(200).send({
+      success: false,
+      msg: e.message
+    })
+  }
+}

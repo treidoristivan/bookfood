05d662fcaeae9e15adee53820e90274b2e51d334
treidoristivan <ristivantreido@gmail.com>
2020-03-08 14:57:27 +0700

    rename object of array failure before

--- src/controllers/itemCategories.js
+++ src/controllers/itemCategories.js
@@ -1,7 +1,7 @@
 const qs = require('qs')
-const { GetCategory, CreateCategory, UpdateCategory, DeleteCategory } = require('../models/itemCategories')
+const { GetCategories, CreateCategories, UpdateCategories, DeleteCategories } = require('../models/itemCategories')
 
-exports.GetAllCategory = async (req, res, next) => {
+exports.GetAllCategories = async (req, res, next) => {
   try {
     const params = {
       currentPage: req.query.page || 1,
@@ -28,9 +28,9 @@ exports.GetAllCategory = async (req, res, next) => {
         }
       })
     }
-    const dataCategory = await GetCategory(false, params)
+    const dataCategories = await GetCategories(false, params)
 
-    const totalPages = Math.ceil(dataCategory.total / parseInt(params.perPage))
+    const totalPages = Math.ceil(dataCategories.total / parseInt(params.perPage))
     const query = req.query
     query.page = parseInt(params.currentPage) + 1
     const nextPage = (parseInt(params.currentPage) < totalPages ? process.env.APP_URL.concat(`${req.baseUrl}?${qs.stringify(query)}`) : null)
@@ -43,12 +43,12 @@ exports.GetAllCategory = async (req, res, next) => {
       previousPage,
       totalPages,
       perPage: params.perPage,
-      totalEntries: dataCategory.total
+      totalEntries: dataCategories.total
     }
-    if (dataCategory.results.length > 0) {
+    if (dataCategories.results.length > 0) {
       res.status(200).send({
         success: true,
-        data: dataCategory.results,
+        data: dataCategories.results,
         pagination
       })
     } else {
@@ -67,19 +67,19 @@ exports.GetAllCategory = async (req, res, next) => {
   }
 }
 
-exports.GetDetailCategory = async (req, res, next) => {
+exports.GetDetailCategories = async (req, res, next) => {
   try {
-    const dataCategory = await GetCategory(req.params.id)
-    if (dataCategory) {
+    const dataCategories = await GetCategories(req.params.id)
+    if (dataCategories) {
       res.status(200).send({
         success: true,
-        data: dataCategory
+        data: dataCategories
       })
     } else {
       res.status(200).send({
         success: true,
         data: false,
-        msg: `Category With id ${req.params.id} Not Exists`
+        msg: `Categories With id ${req.params.id} Not Exists`
       })
     }
   } catch (e) {
@@ -91,19 +91,19 @@ exports.GetDetailCategory = async (req, res, next) => {
   }
 }
 
-exports.CreateCategory = async (req, res, next) => {
+exports.CreateCategories = async (req, res, next) => {
   try {
     if (!req.body.name) {
       throw new Error('name is required')
     }
-    const category = await CreateCategory(req.body.name)
-    if (category) {
+    const categories = await CreateCategories(req.body.name)
+    if (categories) {
       res.status(201).send({
         success: true,
-        msg: 'Success Create Category',
+        msg: 'Success Create Categories',
         data: {
           name: req.body.name,
-          id: category
+          id: categories
         }
       })
     } else {
@@ -118,17 +118,17 @@ exports.CreateCategory = async (req, res, next) => {
   }
 }
 
-exports.UpdateCategory = async (req, res, next) => {
+exports.UpdateCategories = async (req, res, next) => {
   try {
     if (!(req.body.name)) {
       throw new Error('name is required')
     }
     const { id } = req.params
-    const update = await UpdateCategory(id, req.body.name)
+    const update = await UpdateCategories(id, req.body.name)
     if (update) {
       res.status(201).send({
         success: true,
-        msg: `Success Update Category With id ${id}`
+        msg: `Success Update Categories With id ${id}`
       })
     }
   } catch (e) {
@@ -139,15 +139,15 @@ exports.UpdateCategory = async (req, res, next) => {
   }
 }
 
-exports.DeleteCategory = async (req, res, next) => {
+exports.DeleteCategories = async (req, res, next) => {
   try {
     const { id } = req.params
-    if (!(await DeleteCategory(id))) {
-      throw new Error(`Failed To Delete Category With id ${id}`)
+    if (!(await DeleteCategories(id))) {
+      throw new Error(`Failed To Delete Categories With id ${id}`)
     }
     res.status(200).send({
       success: true,
-      msg: `Success to Delete Category With id ${id}`
+      msg: `Success to Delete Categories With id ${id}`
     })
   } catch (e) {
     console.log(e)
